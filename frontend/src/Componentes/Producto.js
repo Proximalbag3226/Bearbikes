9bc223d8bd1ed7d48dd2547127bf31e514ff21bd
Mau San <santiago.castillo.mauricio2@gmail.com>
2023-03-14 12:17:16 -0600

    Hola

--- frontend/src/Componentes/Producto.js
+++ frontend/src/Componentes/Producto.js
@@ -6,7 +6,7 @@ function Producto(props){
             <h3 style={{color: "white",
                 textAlign: "center",
                 fontSize: "40px",
-                fontFamily: 'Oranienbaum',
+                fontFamily: 'Roboto',
                 marginBottom: '15px',}}>{props.producto}</h3>
             <h3>
                 <img src={require(`../IMG/${props.img1}.jpg`)}/>
