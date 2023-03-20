f00f858106063e689c32b6c6f45cf4d98e4440a8
Brdn08 <93061195+Brnd08@users.noreply.github.com>
2023-03-19 18:44:16 -0600

    BACKEND | Se modifico el codigo del metodo 'doFilterInternal' de la clase 'JwtAthenticationFilter' para mejorar la legibilidad y mantenibilidad del mismo

--- backend/src/main/java/com/example/bearbikes_react/controller/filter/JwtAuthenticationFilter.java
+++ backend/src/main/java/com/example/bearbikes_react/controller/filter/JwtAuthenticationFilter.java
@@ -2,6 +2,7 @@ package com.example.bearbikes_react.controller.filter;
 
 import com.example.bearbikes_react.model.repository.TokenRepository;
 import com.example.bearbikes_react.model.service.JwtService;
+import com.example.bearbikes_react.utils.token.Token;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -16,53 +17,65 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.Optional;
 
 @Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
-  private final JwtService jwtService;
-  private final UserDetailsService userDetailsService;
+    private final JwtService jwtService;
+    private final UserDetailsService userDetailsService;
+    private final TokenRepository tokenRepository;
 
     public JwtAuthenticationFilter(JwtService jwtService, UserDetailsService userDetailsService, TokenRepository tokenRepository) {
         this.jwtService = jwtService;
         this.userDetailsService = userDetailsService;
         this.tokenRepository = tokenRepository;
     }
-  private final TokenRepository tokenRepository;
 
-  @Override
-  protected void doFilterInternal(
-      @NonNull HttpServletRequest request,
-      @NonNull HttpServletResponse response,
-      @NonNull FilterChain filterChain
-  ) throws ServletException, IOException {
-    System.out.println(request);
-    final String authHeader = request.getHeader("Authorization");
-    final String jwt;
-    final String userEmail;
-    if (authHeader == null ||!authHeader.startsWith("Bearer ")) {
-      filterChain.doFilter(request, response);
-      return;
+    @Override
+    protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain) throws ServletException, IOException {
+
+        String authHeader = request.getHeader("Authorization");
+
+        if (isAuthHeaderValid(authHeader)) { // check if request has a valid auth Header
+            String jwt = authHeader.substring(7);
+            String userEmail = jwtService.extractUsername(jwt);
+
+            if (userEmail != null && areNoActiveUserSession()) { // check if request has a valid email and there are no active sessions
+                UserDetails userDetails = userDetailsService.loadUserByUsername(userEmail);
+
+                if ( isValidTokenForUser(jwt, userDetails)){ // check if the token info coincides with user request info and if the token is still valid
+                    UsernamePasswordAuthenticationToken authToken =
+                            new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+
+                    authToken.setDetails(
+                            new WebAuthenticationDetailsSource().buildDetails(request)
+                    );
+
+                    SecurityContextHolder.getContext().setAuthentication(authToken);
+                }
+            }
+        }
+            filterChain.doFilter(request, response);
     }
-    jwt = authHeader.substring(7);
-    userEmail = jwtService.extractUsername(jwt);
-    if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-      UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
-      var isTokenValid = tokenRepository.findByToken(jwt)
-          .map(t -> !t.isExpired() && !t.isRevoked())
-          .orElse(false);
-      if (jwtService.isTokenValid(jwt, userDetails) && isTokenValid) {
-        UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
-            userDetails,
-            null,
-            userDetails.getAuthorities()
-        );
-        authToken.setDetails(
-            new WebAuthenticationDetailsSource().buildDetails(request)
-        );
-        SecurityContextHolder.getContext().setAuthentication(authToken);
-      }
+
+    private boolean isAuthHeaderValid(String authHeader) {
+        return authHeader != null && (authHeader.startsWith("Bearer"));
     }
-    filterChain.doFilter(request, response);
-  }
+
+    private boolean areNoActiveUserSession() {
+        return SecurityContextHolder.getContext().getAuthentication() == null;
+    }
+
+
+    private boolean isValidTokenForUser(String jwt, UserDetails userDetails) {
+        Optional<Token> tokenOptional = tokenRepository.findByToken(jwt);
+        if (tokenOptional.isPresent()) { // token Optional has a Token object as value
+            Token token = tokenOptional.get();
+            return !token.isExpired() && !token.isRevoked() & jwtService.isTokenValid(jwt, userDetails);
+        } else // tokenOptional has a null value
+            return false;
+    }
+
+
 }
