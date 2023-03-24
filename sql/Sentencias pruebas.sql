e1684ce28837a9965011b62f5d4393eab2b3d395
Brdn08 <93061195+Brnd08@users.noreply.github.com>
2023-03-23 23:54:38 -0600

    BACKEND | Se modifico el proceso de negocio y por tanto añadio un nuevo usuario llamado 'Dueño Comercio' por lo que se introdujo una tabla 'Empresarios' en la cual se guardan los registros de ambos 'dueño taller' y 'dueño comercio'. Además se añadio un tabla 'Establecimientos' en la cual se guarda la informacion de los Talleres y Comercios indicando en un campo el tipo de Establecimiento del que se trata. Se añadio un procedure para realizar debug, se añadieron procedures para añadir dueños de comercio, talleres y comercios implementando el nuevo proceso de negocio. Se modifico el comportamiento de ciertos procedures necesarios debido al cambio realizado y se añadiero sentencias select* para talleres y comercios que muestran toda la información de los mismos haciendo consultas a las diferentes tablas con la que están relacionadas

--- sql/Sentencias pruebas.sql
+++ sql/Sentencias pruebas.sql
@@ -55,3 +55,35 @@ SELECT @idUsuario AS id, @emailUsuario AS email, @passwordUsuario AS pass, @stat
 SELECT * FROM Token;
 SELECT * FROM token WHERE token.id_usuario = 2;
 SELECT * FROM token WHERE token.token= 'ELTOKEN2_1234Z';
+
+-- AÑADIR ESTABLECIMIENTO ---------------------------------------------------------------------------------------------------------------------------------------------------------
+CALL insertar_dueño_taller ('dueñoDeUnTaller@mysql.com', 'password', 'dueño Procedure', 'Paterno','Materno','5615548192', 'UN_RFC_2',  @idUsuarioInsertado);
+SELECT @idUsuarioInsertado AS id_dueño_nuevo;
+SELECT COUNT(*) from Empresarios where id_empresario = @idUsuarioInsertado; -- verificar que el id del usuario sea un empresario
+INSERT INTO direcciones 
+	(calle, numero_exterior, numero_interior, colonia, codigo_postal, alcaldia, ciudad, tipo_direccion) VALUES
+    ('Mar Mediterràneo', '221', 'S/N', 'Nextitla', '11420', 'Miguel Hidalgo', 'CDMX', 'ESTABLECIMIENTO' );
+
+ -- VERIFICAR QUE EXISTA UNA DIRECCION DE ESTABLECIMIENTO CON EL ID INDICADO
+SELECT COUNT(*) from Direcciones where id_direccion = last_insert_id() and tipo_direccion = 'ESTABLECIMIENTO'; 
+
+SELECT * from Direcciones where tipo_direccion = 'ESTABLECIMIENTO'; 
+
+INSERT INTO Establecimientos 
+	(id_dueño_establecimiento, nombre_establecimiento, rfc_moral, tipo_establecimiento, id_direccion) values
+    (@idUsuarioInsertado, 'ESTABLECIMIENTO 1', '0234567890RFC', 'TALLER', last_insert_id());
+SELECT * FROM ESTABLECIMIENTOS;
+
+SELECT 
+	Establecimientos.nombre_establecimiento, Establecimientos.rfc_moral, Establecimientos.tipo_establecimiento, 
+    Personas.nombre as nombre_dueño, Personas.numero_celular as celular_dueño, 
+    Usuarios.email_usuario as correo_dueño,
+    Direcciones.calle , Direcciones.numero_exterior, Direcciones.numero_interior, Direcciones.colonia, Direcciones.codigo_postal, Direcciones.alcaldia, Direcciones.ciudad, Direcciones.tipo_direccion
+FROM
+	Establecimientos, Personas, Usuarios, Direcciones
+WHERE 
+	Establecimientos.id_dueño_establecimiento = Personas.id_persona AND
+    Personas.id_persona = Usuarios.id_usuario AND 
+    Establecimientos.id_direccion = Direcciones.id_direccion;
+    
+SELECT * FROM EMPRESARIOS;
